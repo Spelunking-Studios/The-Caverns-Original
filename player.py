8f835f7fa98bb49393e6351db4dac4161e792b4d
Ben Landon <81256789+https123456789@users.noreply.github.com>
2022-08-22 07:02:00 -0700

    Added regen

--- player.py
+++ player.py
@@ -30,6 +30,7 @@ class Player(pygame.sprite.Sprite):
         self.imgSheet = {"default": asset('player//samplePlayer.png'), 'hit':asset('player/playerHit1.png'), 'wand':asset('player/playerHit1.png')}
         self.width, self.height = 42, 42
         self.health = 50
+        self.healthAccumulator = 0
 
         self.groups = [game.sprites, game.layer2]
         pygame.sprite.Sprite.__init__(self, self.groups)
@@ -66,6 +67,11 @@ class Player(pygame.sprite.Sprite):
 
     #### Updates player ####
     def update(self):
+        self.healthAccumulator += self.game.dt()
+        if self.healthAccumulator > 1:
+            if self.stats.health < 50:
+                self.stats.health += 0.1 * (50 - self.stats.health)
+            self.healthAccumulator = 0
         self.move()
         self.setAngle()
         self.checkActions()
