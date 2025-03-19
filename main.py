a9cf91701870e8054fae0f3c6f29cb96c572036c
Ben Landon <landb0x207@gmail.com>
2025-03-18 18:56:48 -0700

    feat: optimize inventory system + profiling scaffolding (#37)

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
 from src.game import Game
 
 while __name__ == '__main__':
     game = Game()
-    game.run()
+
+    if os.environ.get("ENABLE_PROFILES", False):
+        import cProfile
+        cProfile.run("game.run()", filename="game.prof", sort="cumtime")
+    else:
+        game.run()
