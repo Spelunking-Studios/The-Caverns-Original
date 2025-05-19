80d72019f8a81044df2e5d82ddf087a9f690b369
LGgameLAB <lukegonsalves07@gmail.com>
2025-05-19 12:46:51 -0700

    fix: add compatibility with old enemy

--- src/enemies/enemy.py
+++ src/enemies/enemy.py
@@ -180,3 +180,6 @@ class Enemy(util.Sprite):
         if int(vec1.x) == int(vec2.x) and int(vec1.y) == int(vec2.y):
             return True
         return False
+
+    def take_knockback(self, other):
+        pass
