2cfdea3bb9826ce7734fcdc30793325227883b01
Ben Landon <landb0x207@gmail.com>
2025-12-21 18:49:03 -0800

    feat(packaging): add windows packaging

--- build.bat
+++ build.bat
@@ -1,15 +1,10 @@
-@REM Attach mode is needed for the console mode in order to not trigger Windows Defender
-uv run nuitka ^
-    --mode=standalone ^
-    --include-data-dir=assets=assets ^
-    --include-data-files=./.venv/Lib/site-packages/pygame/libpng16-16.dll=libpng16-16.dll ^
-    --include-data-files=./.venv/Lib/site-packages/pygame/libjpeg-62.dll=libjpeg-62.dll ^
-    --lto=yes ^
-    --jobs=18 ^
-    --windows-company-name="Spelunking Studios" ^
-    --windows-product-name="The Caverns" ^
-    --windows-file-version="0.0.1" ^
-    --windows-product-version="0.0.1" ^
-    --windows-file-description="A top down indie action-adventure RPG" ^
-    --windows-console-mode="attach" ^
-    main.py
+uv run pyinstaller main.py ^
+    --onefile ^
+    --name "The Caverns" ^
+    --noconfirm ^
+    --add-data assets:assets ^
+    --hidden-import OpenGL.platform.egl ^
+    --collect-all pygame_render ^
+    --collect-all pygame_light2d
+cd packaging\windows
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" msi.iss
